bd25178a9214e62edf96e49d745741635ce1bc54
Jonas Debes <jpdebes@gmail.com>
2018-02-13 23:46:25 +1300

    Move fileupload to function to be called from goroutine. To prevent closure access to shared variables from outside.

--- slurp/slurp_cws_images.go
+++ slurp/slurp_cws_images.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/jdebes/slurpCwsImages-lambda/model"
 	"github.com/jdebes/slurpCwsImages-lambda/service"
 )
 
@@ -29,29 +30,31 @@ func SlurpImages(cwsService service.CwsService, awsService service.AwsService) {
 			if strings.ToUpper(region) == "WORLDWIDE" {
 				for _, image := range item.Images {
 					sem <- struct{}{}
-					go func() {
-						defer wg.Done()
-						defer func() { <-sem }()
-
-						fileUrl, err := cwsService.HeadProductImageForUrl(image.Image)
-						if err != nil || fileUrl == noImageUrl {
-							return
-						}
-						fileExt := filepath.Ext(fileUrl)
-
-						if !awsService.S3ItemExists(item.ProductID, fileExt, image.Format) {
-							file, _, err := cwsService.GetProductImage(image.Image)
-							if err != nil {
-								return
-							}
-
-							awsService.UploadItemToS3(file, fileExt, item.ProductID, image.Format)
-						}
-					}()
+					go uploadImage(wg, sem, image, item, cwsService, awsService)
 				}
 			}
 		}
 	}
 
 	wg.Wait()
+}
+
+func uploadImage(wg sync.WaitGroup, semaphore chan struct{}, image model.Image, item model.CwsProduct, cwsService service.CwsService, awsService service.AwsService) {
+	defer wg.Done()
+	defer func() { <-semaphore }()
+
+	fileUrl, err := cwsService.HeadProductImageForUrl(image.Image)
+	if err != nil || fileUrl == noImageUrl {
+		return
+	}
+	fileExt := filepath.Ext(fileUrl)
+
+	if !awsService.S3ItemExists(item.ProductID, fileExt, image.Format) {
+		file, _, err := cwsService.GetProductImage(image.Image)
+		if err != nil {
+			return
+		}
+
+		awsService.UploadItemToS3(file, fileExt, item.ProductID, image.Format)
+	}
 }
